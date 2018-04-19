57618a6edfc4b6c05e8b997cd6731f14921deef6
Ravela Smyth <RavSmy@users.noreply.github.com>
2018-04-19 07:30:02 -0700

    Update Fraction.h

--- Lab #2,3,4 - Smyth/Fraction.h
+++ Lab #2,3,4 - Smyth/Fraction.h
@@ -1,3 +1,5 @@
+// Smyth, Ravela -- CS 540 -- Lab #2+3+4 --Fraction.h 
+
 #ifndef FRACTION_H
 #define FRACTION_H
 
@@ -26,13 +28,18 @@ public:
 	int getNumer() const { return numer; }
 	int getWhole() const { return whole; }
 
-	//**LAB 3** --- Constructor + Destructor
+	//**LAB 3** --- Copy Constructor + Destructor
 	Fraction(const Fraction &obj);
 	~Fraction();
 
 	//**LAB 4** --- Operator Overloading
 	Fraction operator+(const Fraction& obj);
 	Fraction operator*(const Fraction& obj);
+	bool operator==(const Fraction& obj);
+	void operator=(const Fraction& obj);
+
+	int gcd(int a, int b);
+	int add_whole_To_num(const Fraction* obj);
 };
 
 #endif
