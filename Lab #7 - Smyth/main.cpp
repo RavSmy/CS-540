f8ab875722fe3b24192ae13b762707c68660117c
Ravela Smyth <RavSmy@users.noreply.github.com>
2018-05-07 23:40:38 -0700

    Add files via upload

--- Lab #7 - Smyth/main.cpp
+++ Lab #7 - Smyth/main.cpp
@@ -1,22 +1,13 @@
 #include <iostream>
-#include "Elf.h"
-#include "Human.h"
 #include "Creature.h"
 #include "Demon.h"
-#include "Balrogs.h"
-#include "Cyberdemon.h"
 
 int main()
 {
-	Human human1;
-	Elf elf1;
-	Balrogs balrogs1;
-	Cyberdemon cd1;
+	Demon demon;
+	Creature creature;  
 	
-	std::cout << "human1.getDamage(): " << human1.getDamage()
-	     << "\nelf1.getDamage(): " << elf1.getDamage()
-	     << "\nbalrogs1.getDamage(): " << balrogs1.getDamage()
-	     << "\ncyberdemon1.getDamage(): " << cd1.getDamage()
-	     << "\n";
+	for (int i = 0 ; i < 10 ; i++) std::cout << "\ndemon.getDamage(): " << demon.getDamage(); 
+	for (int i = 0 ; i < 10 ; i++) std::cout << "\ncreature.getDamage(): " << creature.getDamage(); 
 }
 
