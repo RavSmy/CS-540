37f0635e9b355dba9b388d9d5669a477359880e3
Ravela Smyth <RavSmy@users.noreply.github.com>
2018-05-08 10:45:40 -0700

    Add files via upload

---
+++ Lab #5 - Smyth/Fraction.cpp
@@ -0,0 +1,93 @@
+// Smyth, Ravela -- CS 540 TH. --  Lab #2+3+4+5+6 --Fraction.cpp 
+
+#include "Fraction.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/*LAB #2*/
+void Fraction::check() {
+
+	if (denom == 0) { denom = 1; cout << "-->Denominator changed to 1, it can't be 0.\n"; }
+	if (numer < 0 || denom < 0 || whole < 0) {
+		cout << "-->Your values were negative, now absolute value.\n";
+		setAll(abs(whole), abs(numer), abs(denom));
+	}
+
+	if (numer > denom) {
+		whole += numer / denom;
+		numer = numer % denom;
+	}
+}
+
+Fraction::Fraction() { setAll(0, 0, 1); }
+
+Fraction::Fraction(int n, int d) { setAll(0, n, d); }
+
+Fraction::Fraction(int w, int n, int d) { setAll(w, n, d); }
+
+void Fraction::print() {
+	cout << "\nF: ";
+	if (whole != 0) cout << whole << " ";
+	cout << numer << "/" << denom << '\n';
+}
+
+void Fraction::setAll(int w, int n, int d) {
+	denom = d; whole = w; numer = n;
+	check();
+}
+
+/*LAB #3 -- Copy Constructor + Destructor*/
+
+Fraction::Fraction(const Fraction &obj) {
+	cout << "-->Copy Constructor Called...\n";
+	setAll(obj.whole, obj.numer, obj.denom);
+}
+
+Fraction::~Fraction() {
+	cout << "-->Destructor Called...\n";
+	setAll(0, 0, 1);
+}
+
+/*LAB #4 -- Operation Overloading*/
+Fraction Fraction::operator+(const Fraction& obj) {
+	int denominator = obj.denom * denom;
+
+	int numerator = (add_whole_To_num(this) * obj.denom) +
+		(add_whole_To_num(&obj) * denom);
+
+	Fraction sum(0, numerator, denominator);
+	return sum;
+}
+
+Fraction Fraction::operator*(const Fraction& obj) {
+	int numerator = add_whole_To_num(&obj) * add_whole_To_num(this)
+		, denominator = obj.denom * denom;
+
+	Fraction mult(0, numerator, denominator);
+	return mult;
+}
+bool Fraction::operator==(const Fraction& obj) {
+	return (add_whole_To_num(this) * obj.denom) == (add_whole_To_num(&obj) * denom);
+}
+
+void Fraction::operator=(const Fraction& obj) {
+	setAll(obj.whole, obj.numer, obj.denom);
+}
+
+int Fraction::add_whole_To_num(const Fraction* obj) {
+	return (obj->whole * obj->denom) + obj->numer;
+}
+
+	
+ostream& operator<<(ostream &os, const Fraction& obj) {
+	os << obj.whole << " " << obj.numer << "/" << obj.denom;
+	return os;  
+} 
+
+istream& operator>>(istream &is, Fraction& obj) { 
+	char slash;
+	is >> obj.whole >> obj.numer >> slash >> obj.denom;
+ 	obj.check();
+	return is; 
+}
---
+++ Lab #5 - Smyth/main.cpp
@@ -0,0 +1,84 @@
+// Smyth, Ravela -- CS 540 TH. -- Lab #2+3+4+5+6 -- main.cpp
+
+#include <iostream>
+#include "Fraction.h"
+
+using namespace std;
+
+int main() {
+
+	/*LAB #2*/
+
+	//NO ARGS--default: denom = 1, numer = 0, whole = 0
+	Fraction *myfrac1 = new (nothrow) Fraction();
+	if (myfrac1 != NULL) {
+		myfrac1->print();
+		cout << "   [frac2: 0/1]\n\n";
+
+		myfrac1->setWhole(-10);
+		myfrac1->setDenom(0);
+		myfrac1->setNumer(1);
+		myfrac1->print();
+		cout << "   [frac1: 11 0/1]\n\n";
+	}
+
+	// Free Pointer
+	delete myfrac1;
+	myfrac1 = NULL;
+
+
+	//2 ARGS--format: (numerator denominator)
+	Fraction myfrac2(1, 8);
+	myfrac2.print();
+	cout << "   [frac2: 1/8]\n\n";
+
+
+	//THREE ARGS--format: (whole numerator denominator)
+	Fraction myfrac3(1, 8, 3);
+	myfrac3.print();
+	cout << "   [frac3: 3 2/3]\n\n";
+
+	/*LAB #3*/
+
+	//Copy Constructor
+	Fraction myfrac4 = myfrac3;
+	myfrac4.print();
+	cout << "   [frac4 = frac3 : 3 2/3]\n\n";
+
+	//Destructor
+	myfrac4.~Fraction();
+	myfrac4.print();
+	cout << "   [~frac4: 0/1]\n\n";
+
+	/*LAB #4*/
+
+	Fraction myfrac5;
+
+	//Operator Overloadings: +
+	myfrac5 = myfrac3 + myfrac2;
+	myfrac5.print();
+	cout << "   [frac5 = frac3 (3 2/3) + frac2 (1/8) : 3 19/24]\n\n";
+
+	//Operator Overloadings: *
+	myfrac5 = myfrac3 * myfrac2;
+	myfrac5.print();
+	cout << "   [frac5 = frac3 (3 2/3) * frac2 (1/8) : 11/24]\n\n";
+
+	//Operator Overloadings: ==
+	Fraction myfrac6(3, 4, 6);
+	cout << "\n   [frac6 (3 4/6) and frac3 (3 2/3) are: ";
+	if (myfrac6 == myfrac3) cout << "EQUAL\n\n";
+	else cout << "NOT EQUAL\n\n";
+
+	Fraction myfrac7(3, 4, 3);
+	cout << "\n   [frac7 (3 4/1) and frac3 (3 2/3) are: ";
+	if (myfrac7 == myfrac3) cout << "EQUAL\n\n";
+	else cout << "NOT EQUAL\n\n";
+
+	/*LAB #5 -- << >> Overloading*/
+	Fraction myfrac8;  
+	cout << "Input frac8 (w n/d): ";	
+	cin >> myfrac8; 
+	cout << "Your frac8 : " << myfrac8 << "\n"; 
+
+}   
