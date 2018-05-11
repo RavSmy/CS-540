aa26dbc82ba9bbe367fe09d0555fcfc9ab31cf7f
Ravela Smyth <RavSmy@users.noreply.github.com>
2018-05-10 18:30:38 -0700

    Create Fraction.cpp

---
+++ Lab -9 - Smyth/Fraction.cpp
@@ -0,0 +1,87 @@
+// Smyth, Ravela -- CS 540 TH. --  Lab #9 --Fraction.cpp 
+
+#include "Fraction.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void Fraction::check() {
+
+	if (denom == 0) { denom = 1; cout << "-->Denominator changed to 1, it can't be 0.\n"; }
+	if (numer < 0 || denom < 0 || whole < 0) {
+		cout << "-->Your values were negative, now absolute value.\n";
+		setAll(abs(whole), abs(numer), abs(denom));
+	}
+
+	if (numer > denom) {
+		whole += numer / denom;
+		numer = numer % denom;
+	}
+}
+
+Fraction::Fraction() { setAll(0, 0, 1); }
+
+Fraction::Fraction(int n, int d) { setAll(0, n, d); }
+
+Fraction::Fraction(int w, int n, int d) { setAll(w, n, d); }
+
+void Fraction::print() const {
+	if (whole != 0) cout << whole << " ";
+	cout << numer << "/" << denom;
+}
+
+void Fraction::setAll(int w, int n, int d) {
+	denom = d; whole = w; numer = n;
+	check();
+}
+
+Fraction::Fraction(const Fraction &obj) {
+	cout << "-->Copy Constructor Called...\n";
+	setAll(obj.whole, obj.numer, obj.denom);
+}
+
+Fraction::~Fraction() {
+	cout << "-->Destructor Called...\n";
+	setAll(0, 0, 1);
+}
+
+Fraction Fraction::operator+(const Fraction& obj) {
+	int denominator = obj.denom * denom;
+
+	int numerator = (add_whole_To_num(this) * obj.denom) +
+		(add_whole_To_num(&obj) * denom);
+
+	Fraction sum(0, numerator, denominator);
+	return sum;
+}
+
+Fraction Fraction::operator*(const Fraction& obj) {
+	int numerator = add_whole_To_num(&obj) * add_whole_To_num(this)
+		, denominator = obj.denom * denom;
+
+	Fraction mult(0, numerator, denominator);
+	return mult;
+}
+bool Fraction::operator==(const Fraction& obj) {
+	return (add_whole_To_num(this) * obj.denom) == (add_whole_To_num(&obj) * denom);
+}
+
+void Fraction::operator=(const Fraction& obj) {
+	setAll(obj.whole, obj.numer, obj.denom);
+}
+
+int Fraction::add_whole_To_num(const Fraction* obj) {
+	return (obj->whole * obj->denom) + obj->numer;
+}
+
+ostream& operator<<(ostream &os, const Fraction& obj) {
+	obj.print();
+	return os;
+}
+
+istream& operator>>(istream &is, Fraction& obj) {
+	char slash;
+	is >> obj.whole >> obj.numer >> slash >> obj.denom;
+	obj.check();
+	return is;
+}
