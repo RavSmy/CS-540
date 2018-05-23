1830db4e874da2a4514cb8c7457c13969a74aff0
Rav Smy <RavSmy@users.noreply.github.com>
2018-05-23 13:07:19 -0700

    Update Creature.cpp

--- Lab #7 - Smyth/Creature.cpp
+++ Lab #7 - Smyth/Creature.cpp
@@ -13,17 +13,17 @@ Creature::Creature(int newStrength, int newHit) {
 	strength = newStrength;
 }
 
-int Creature::getDamage() {
+int Creature::getDamage()  const{
 	int damage = (rand() % strength) + 1;
 	std::cout << std::endl << getSpecies() << " attacks for " << damage << " points!" << std::endl;
 	return damage;
 }
 
-std::string Creature::getSpecies() {
+std::string Creature::getSpecies()  const{
 	return "Creature";
 }
 
-int Creature::getStrength() { return strength; }
-int Creature::getHitpoints() { return hitpoints; }
+int Creature::getStrength()  const { return strength; }
+int Creature::getHitpoints()  const { return hitpoints; }
 void Creature::setStrength(int strength) { this->strength = strength; }
 void Creature::setHitpoints(int hitpoints) { this->hitpoints = hitpoints; }
